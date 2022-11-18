d20135c30978a52cdb58d8415cfd88cee930ae98
Vadim Savin <savinvadim1312@gmail.com>
2022-11-18 15:54:00 +0000

    Install and setup Apollo Client

--- App.tsx
+++ App.tsx
@@ -1,9 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+const API_KEY =
+  "lalmanirhat::stepzen.net+1000::204fe77487127b04491414e08cc7ef94aae4dd7af37dd0bd9e0856ddfdbf4229";
+
+const client = new ApolloClient({
+  uri: "https://lalmanirhat.stepzen.net/api/nonexistent-camel/__graphql",
+  headers: {
+    Authorization: `Apikey ${API_KEY}`,
+  },
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
@@ -14,7 +26,9 @@ export default function App() {
   } else {
     return (
       <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
+        <ApolloProvider client={client}>
+          <Navigation colorScheme={colorScheme} />
+        </ApolloProvider>
         <StatusBar />
       </SafeAreaProvider>
     );
